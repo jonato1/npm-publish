b7027063498f005650dbf34d36b4bd439c2369b3
Facundo Allemand <facundo.allemand@getyourguide.com>
2021-10-06 22:54:34 +0200

    refactor code

--- npm-publish.js
+++ npm-publish.js
@@ -8,7 +8,7 @@ import { getParams, shouldBuildVersion, getNewVersion, createNewVersion, pushToG
  */
 const {
   message: fullMessage,
-  branch,
+  branch: fullBranch,
   publishBranches,
   wildcardBeta,
   wildcardMajor,
@@ -26,12 +26,12 @@ const parentPackage = readPackageUpSync().packageJson;
 const buildBeta = message.toLowerCase().includes(wildcardBeta);
 // This exists since GITHUB_REF provided by GitHub Actions, unlike
 // DRONE_BRANCH, CIRCLE_BRANCH, etc contains the `refs/heads/` prefix
-const curatedBranch = branch.replace('refs/heads/', '');
+const branch = fullBranch.replace('refs/heads/', '');
 
 console.info('-------------------------------------------');
 console.info('-------------   NPM PUBLISH   -------------');
 console.info('-------------------------------------------');
-console.info(`BRANCH: ${curatedBranch}`);
+console.info(`BRANCH: ${branch}`);
 console.info(`MESSAGE: "${message}"`);
 console.info(`WILDCARD_MAJOR: "${wildcardMajor}"`);
 console.info(`WILDCARD_MINOR: "${wildcardMinor}"`);
@@ -40,7 +40,7 @@ console.info(`WILDCARD_NO_PUBLISH: "${wildcardNoPublish}"`);
 console.info(`PUBLISH BRANCHES: ${publishBranches}\n`);
 
 // 1. Check if we should build the version
-if (!shouldBuildVersion(publishBranches, curatedBranch, message, wildcardNoPublish, buildBeta)) {
+if (!shouldBuildVersion(publishBranches, branch, message, wildcardNoPublish, buildBeta)) {
   console.info('[NPM-PUBLISH] Exit: No need to build the version');
   process.exit(0);
 }
@@ -54,8 +54,8 @@ console.info(`npm i --save ${parentPackage.name}@${newVersion}`);
 console.info('---------------------------------\n');
 
 // 3. Set up GIT
-console.info('[NPM-PUBLISH] Config GIT');
-setupGit(curatedBranch, gitEmail, gitName);
+console.info('[NPM-PUBLISH] Reset changes & config git');
+// setupGit(branch, gitEmail, gitName);
 
 // 4. Create and publish new version
 console.info('[NPM-PUBLISH] Creating new version');
@@ -70,5 +70,5 @@ try {
 // 5. Push changes to Git repository
 if (!buildBeta) {
   console.info(`[NPM-PUBLISH] Pushing tag & commit to repository`);
-  pushToGitRepo(curatedBranch, parentPackage, commitMessage, tagName);
+  pushToGitRepo(branch, parentPackage, commitMessage, tagName);
 }
